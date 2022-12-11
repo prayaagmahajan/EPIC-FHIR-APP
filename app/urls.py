9a029546a6b351b376f66c133a88ea28af519af6
prayaag mahajan <prayaagmahajan@gmail.com>
2022-12-11 16:04:41 +0530

    new changes added

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,11 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',views.home,name='home' ),
-    path('observation/',views.observation,name='observation' ),
-    path('encounter/',views.encounter,name='encounter' ),
+    # path('observation/',views.observation,name='observation' ),
+    # path('encounter/',views.encounter,name='encounter' ),
     path('jsonviewPatient/<id>/',views.jsonviewPatient, name='jsonviewPatient' ),
-    path('jsonviewObservation/<id>/', views.jsonviewObservation, name='jsonviewObservation'),
-    path('jsonviewEncounter/<id>/', views.jsonviewEncounter, name='jsonviewEncounter'),
+    # path('jsonviewObservation/<id>/', views.jsonviewObservation, name='jsonviewObservation'),
+    # path('jsonviewEncounter/<id>/', views.jsonviewEncounter, name='jsonviewEncounter'),
     path('url/', views.url, name='url'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render , get_object_or_404
-from operator import itemgetter
 import requests
-import json
-import adal
 from django.http import JsonResponse
 from django.core.paginator import Paginator
 
@@ -17,22 +14,7 @@ Resource=""
 def home(request):
     # oauth 2.0 --FHIR SERVER AUTHORIZATION
     # Opening JSON file
-    f = open('app/credentials.json')
-    data = json.load(f)
-    TENANT_ID = data.get('TENANT_ID')
-    CLIENT = data.get('CLIENT_ID')
-    KEY = data.get('CLIENT_SECRET')
-    authority_url = 'https://login.microsoftonline.com/' + TENANT_ID
-    global Resource
-    Resource=data.get('RESOURCE_ID')
-    context = adal.AuthenticationContext(authority_url)
-    global token
-    token = context.acquire_token_with_client_credentials(
-        resource=Resource,
-        client_id=CLIENT,
-        client_secret=KEY)
-    global TOKEN
-    TOKEN = token["accessToken"]
+    Resource="http://hapi.fhir.org/baseR4/"
 
     # Get Patient by id
     if request.method == "POST":
@@ -40,8 +22,7 @@ def home(request):
         global pid
         pid = request.POST.get('ID', '')
         url = Resource+"/Patient/{}".format(pid)
-        newHeaders = {'Content-type': 'application/json',"Authorization": "Bearer %s" %TOKEN}
-        response = requests.get(url, headers=newHeaders,verify=False)
+        response = requests.get(url)
         data = response.json()
         if response.ok:
             l = []
@@ -76,12 +57,12 @@ def home(request):
             l.append(address)
             lst.append(l)
         param = {'param': lst,"id":pid}
+        print(param)
         return render(request, 'app/home.html', param)
     #all patients
     elif request.method == 'GET':
-        url = Resource+"/Patient?_count=20"
-        newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" %TOKEN}
-        response = requests.get(url, headers=newHeaders, verify=False)
+        url = Resource+"/Patient?_count=5"
+        response = requests.get(url)
         data = response.json()
         lst = []
         # next_pge_url = response.json()['link'][0].get('url')
@@ -147,157 +128,157 @@ def home(request):
             param = {'param':lst}
             return render(request, 'app/home.html', param)
 
-def observation(request):
-    lst = []
-    id = pid
-    if id is None:
-        obsparam = {'obsparam': "No patient id found"}
-        return render(request, 'app/home.html', obsparam)
-    else:
-        url = Resource+"/Observation?patient={}".format(id)
-        newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" %TOKEN}
-        response = requests.get(url, headers=newHeaders,verify=False)
-        print(response.json())
-        if response.ok:
-            data = response.json()
-            if 'entry' not in data.keys():
-                print("NO Observation available")
-            else:
-                entry = data['entry']
-                for all_data in entry:
-                    l = []
-                    resourceType = all_data['resource']['resourceType']
-                    l.append(resourceType)
-
-                    id = all_data['resource']['id']
-                    global observation_id
-                    observation_id = id
-                    l.append(id)
-
-                    try:
-                        reference = all_data['resource']['subject'].get('reference')
-                    except:
-                        reference = "No data available"
-                    l.append(reference)
-
-                    try:
-                        display = all_data['resource']['code']['coding'][0].get('display')
-                    except:
-                        display = "No data available"
-                    l.append(display)
-
-                    try:
-                        res = list(map(itemgetter('coding'), all_data['resource']['category']))
-                        category = res[0][0]['display']
-                    except:
-                        category = "No data available"
-                    l.append(category)
-
-                    try:
-                        v1 = all_data['resource']['valueQuantity'].get('value')
-                        v2 = all_data['resource']['valueQuantity'].get('unit')
-                        value = str(v1) + " " + str(v2)
-                    except:
-                        value = "No data available"
-                    l.append(value)
-                    lst.append(l)
-        obsparam = {'obsparam': lst,'observation_id':observation_id}
-        return render(request, 'app/home.html', obsparam)
-
-def encounter(request):
-    lst = []
-    if pid is None:
-        obsparam = {'obsparam': "No patient id found"}
-        return render(request, 'app/home.html', obsparam)
-    else:
-        url = Resource+"/Encounter?patient={}".format(pid)
-        newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" % TOKEN}
-        response = requests.get(url, headers=newHeaders,verify=False)
-        if response.ok:
-            data = response.json()
-            if 'entry' not in data.keys():
-                print("NO Encounter available")
-            else:
-                entry = data['entry']
-                for all_data in entry:
-                    l = []
-                    try:
-                        resourceType = all_data['resource']['resourceType']
-                    except:
-                        resourceType = "No data available"
-                    l.append(resourceType)
-
-                    try:
-                        id = all_data['resource']['id']
-                        global encounter_id
-                        encounter_id=id
-                    except:
-                        id = "No data available"
-                    l.append(id)
-
-                    try:
-                        priority = all_data['resource']['priority']['coding'][0]['display']
-                    except:
-                        priority = "No data available"
-                    l.append(priority)
-
-                    try:
-                        reason = list(map(itemgetter('coding'), all_data['resource']['reasonCode']))
-                        reasonCode = reason[0][0]['display']
-                    except:
-                        reasonCode = "No data available"
-                    l.append(reasonCode)
-
-                    try:
-                        admitSource = all_data['resource']['hospitalization']['admitSource']['coding'][0]['display']
-                    except:
-                        admitSource = "No data available"
-                    l.append(admitSource)
-
-                    try:
-                        serviceProvider = all_data['resource']['serviceProvider'].get('reference')
-                    except:
-                        serviceProvider = "No data available"
-                    l.append(serviceProvider)
-
-                    lst.append(l)
-        encounter_param = {'encounter_param': lst,'encounter_id':encounter_id}
-        return render(request, 'app/home.html', encounter_param)
+# def observation(request):
+#     lst = []
+#     id = pid
+#     if id is None:
+#         obsparam = {'obsparam': "No patient id found"}
+#         return render(request, 'app/home.html', obsparam)
+#     else:
+#         url = Resource+"/Observation?patient={}".format(id)
+#         newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" %TOKEN}
+#         response = requests.get(url, headers=newHeaders,verify=False)
+#         print(response.json())
+#         if response.ok:
+#             data = response.json()
+#             if 'entry' not in data.keys():
+#                 print("NO Observation available")
+#             else:
+#                 entry = data['entry']
+#                 for all_data in entry:
+#                     l = []
+#                     resourceType = all_data['resource']['resourceType']
+#                     l.append(resourceType)
+#
+#                     id = all_data['resource']['id']
+#                     global observation_id
+#                     observation_id = id
+#                     l.append(id)
+#
+#                     try:
+#                         reference = all_data['resource']['subject'].get('reference')
+#                     except:
+#                         reference = "No data available"
+#                     l.append(reference)
+#
+#                     try:
+#                         display = all_data['resource']['code']['coding'][0].get('display')
+#                     except:
+#                         display = "No data available"
+#                     l.append(display)
+#
+#                     try:
+#                         res = list(map(itemgetter('coding'), all_data['resource']['category']))
+#                         category = res[0][0]['display']
+#                     except:
+#                         category = "No data available"
+#                     l.append(category)
+#
+#                     try:
+#                         v1 = all_data['resource']['valueQuantity'].get('value')
+#                         v2 = all_data['resource']['valueQuantity'].get('unit')
+#                         value = str(v1) + " " + str(v2)
+#                     except:
+#                         value = "No data available"
+#                     l.append(value)
+#                     lst.append(l)
+#         obsparam = {'obsparam': lst,'observation_id':observation_id}
+#         return render(request, 'app/home.html', obsparam)
+#
+# def encounter(request):
+#     lst = []
+#     if pid is None:
+#         obsparam = {'obsparam': "No patient id found"}
+#         return render(request, 'app/home.html', obsparam)
+#     else:
+#         url = Resource+"/Encounter?patient={}".format(pid)
+#         newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" % TOKEN}
+#         response = requests.get(url, headers=newHeaders,verify=False)
+#         if response.ok:
+#             data = response.json()
+#             if 'entry' not in data.keys():
+#                 print("NO Encounter available")
+#             else:
+#                 entry = data['entry']
+#                 for all_data in entry:
+#                     l = []
+#                     try:
+#                         resourceType = all_data['resource']['resourceType']
+#                     except:
+#                         resourceType = "No data available"
+#                     l.append(resourceType)
+#
+#                     try:
+#                         id = all_data['resource']['id']
+#                         global encounter_id
+#                         encounter_id=id
+#                     except:
+#                         id = "No data available"
+#                     l.append(id)
+#
+#                     try:
+#                         priority = all_data['resource']['priority']['coding'][0]['display']
+#                     except:
+#                         priority = "No data available"
+#                     l.append(priority)
+#
+#                     try:
+#                         reason = list(map(itemgetter('coding'), all_data['resource']['reasonCode']))
+#                         reasonCode = reason[0][0]['display']
+#                     except:
+#                         reasonCode = "No data available"
+#                     l.append(reasonCode)
+#
+#                     try:
+#                         admitSource = all_data['resource']['hospitalization']['admitSource']['coding'][0]['display']
+#                     except:
+#                         admitSource = "No data available"
+#                     l.append(admitSource)
+#
+#                     try:
+#                         serviceProvider = all_data['resource']['serviceProvider'].get('reference')
+#                     except:
+#                         serviceProvider = "No data available"
+#                     l.append(serviceProvider)
+#
+#                     lst.append(l)
+#         encounter_param = {'encounter_param': lst,'encounter_id':encounter_id}
+#         return render(request, 'app/home.html', encounter_param)
 
 def jsonviewPatient(request,id):
     id = str(id)
+    Resource = "http://hapi.fhir.org/baseR4/"
     url = Resource+"/Patient/{}".format(id)
-    newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" % TOKEN}
-    response = requests.get(url, headers=newHeaders,verify=False)
+    response = requests.get(url)
     if response.ok:
         json_data = response.json()
         # json_formatted_patient= json.dumps(json_data, sort_keys = True, indent = 4)
     # param = {'param':json_formatted_patient}
     return JsonResponse(json_data,content_type='application/json')
 
-def jsonviewObservation(request,id):
-    id = str(id)
-    url = Resource+"/Observation/{}".format(id)
-    newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" % TOKEN}
-    response = requests.get(url, headers=newHeaders,verify=False)
-    if response.ok:
-        json_data = response.json()
-    #     json_formated_observation=json.dumps(json_data, sort_keys = True, indent = 4)
-    # param = {'param':json_formated_observation}
-    # return render(request,'app/jsondata.html',param)
-    return JsonResponse(json_data,content_type='application/json')
-
-def jsonviewEncounter(request,id):
-    id = str(id)
-    url = Resource+"/Encounter/{}".format(id)
-    newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" % TOKEN}
-    response = requests.get(url, headers=newHeaders,verify=False)
-    if response.ok:
-        json_data = response.json()
-    #     json_formated_encounter=json.dumps(json_data, sort_keys = True, indent = 4)
-    # param = {'param':json_formated_encounter}
-    # return render(request,'app/jsondata.html',param)
-    return JsonResponse(json_data,content_type='application/json')
+# def jsonviewObservation(request,id):
+#     id = str(id)
+#     url = Resource+"/Observation/{}".format(id)
+#     newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" % TOKEN}
+#     response = requests.get(url, headers=newHeaders,verify=False)
+#     if response.ok:
+#         json_data = response.json()
+#     #     json_formated_observation=json.dumps(json_data, sort_keys = True, indent = 4)
+#     # param = {'param':json_formated_observation}
+#     # return render(request,'app/jsondata.html',param)
+#     return JsonResponse(json_data,content_type='application/json')
+#
+# def jsonviewEncounter(request,id):
+#     id = str(id)
+#     url = Resource+"/Encounter/{}".format(id)
+#     newHeaders = {'Content-type': 'application/json', "Authorization": "Bearer %s" % TOKEN}
+#     response = requests.get(url, headers=newHeaders,verify=False)
+#     if response.ok:
+#         json_data = response.json()
+#     #     json_formated_encounter=json.dumps(json_data, sort_keys = True, indent = 4)
+#     # param = {'param':json_formated_encounter}
+#     # return render(request,'app/jsondata.html',param)
+#     return JsonResponse(json_data,content_type='application/json')
 
 def url(request):
     if request.method == "POST":
